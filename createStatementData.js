d81f354882ee28fc11c31ec0231d018be5fbaa08
MertensMing <mertenslzm@gmail.com>
2020-06-06 23:27:56 +0800

    提醒后来者记得实现 amount 这个函数

--- createStatementData.js
+++ createStatementData.js
@@ -4,22 +4,7 @@ class PerformanceCalculator {
     this.play = aPlay;
   }
   get amount() {
-    let result = 0;
-    switch (this.play.type) {
-      case "tragedy":
-        throw 'bad thing';
-      case "comedy":
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20
-          );
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${this.play.type}`);
-    }
-    return result;
+    throw new Error('subclass responsibility');
   }
   get volumeCredits() {
     let result = 0;
@@ -39,7 +24,16 @@ class TragedyCalculator extends PerformanceCalculator {
   }
 }
 
-class ComedyCalculator extends PerformanceCalculator {}
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+}
 
 function createPerformanceCalculator(aPerformance, aPlay) {
   switch (aPlay.type) {
