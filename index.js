9876fc8856cfe7e4aec4f6f7a9d314ddf1683586
MertensMing <mertenslzm@gmail.com>
2020-06-06 22:51:17 +0800

    添加渲染 HTML 的代码

--- index.js
+++ index.js
@@ -7,19 +7,39 @@ function statement(invoice, plays) {
 function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
   for (let perf of data.performances) {
-    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats) \n`;
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
   }
   result += `Amount owed is ${usd(data.totalAmount)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
+}
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US",
-      {
-        style: "currency", currency: "USD",
-        minimumFractionDigits: 2
-      }).format(aNumber);
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+  for (let perf of data.performances) {
+    result += `<tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
   }
+  result += "</table>\n";
+  result += `<p>Amount owed is<em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned<em>${data.totalVolumeCredits}</em>credits</p>\n`;
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US",
+    {
+      style: "currency", currency:
+        "USD",
+      minimumFractionDigits: 2
+    }).format(aNumber / 100);
 }
 
 console.log(statement(require('./invoices.json')[0], require('./plays.json')));
+console.log(htmlStatement(require('./invoices.json')[0], require('./plays.json')));
