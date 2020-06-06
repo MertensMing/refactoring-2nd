718d605d1a180011f0a51de200be31882cfc58e0
MertensMing <13680543450@163.com>
2020-06-06 17:13:33 +0800

    在amountFor函数内部使用新提炼的函数

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function playFor(aPerformance) {
 
 function amountFor(aPerformance, play) {
   let result = 0;
-  switch (play.type) {
+  switch (playFor(aPerformance).type) {
     case "tragedy":
       result = 40000;
       if (aPerformance.audience > 30) {
@@ -22,7 +22,7 @@ function amountFor(aPerformance, play) {
       result += 300 * aPerformance.audience;
       break;
     default:
-      throw new Error(`unknown type: ${play.type}`);
+      throw new Error(`unknown type: ${playFor(aPerformance).type}`);
   }
   return result;
 }
