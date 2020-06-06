51d0f98913dd76d78e617ba8bc18889fa4a39d37
MertensMing <mertenslzm@gmail.com>
2020-06-06 17:48:35 +0800

    在提炼函数时先给它随便取了一个名字

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ function volumeCreditsFor(perf) {
   return volumeCredits;
 }
 
+function appleSauce() {
+  let totalAmount = 0;
+  for (let perf of invoice.performances) {
+    totalAmount += amountFor(perf);
+  }
+  return totalAmount;
+}
+
 function amountFor(aPerformance) {
   let result = 0;
   switch (playFor(aPerformance).type) {
@@ -51,13 +59,11 @@ function amountFor(aPerformance) {
 }
 
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
-    // print line for this order
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))}(${perf.audience} seats)\n`;
-    totalAmount += amountFor(perf);
   }
+  let totalAmount = appleSauce();
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
