711296bb8d48aabcf617533268766118b089bec1
MertensMing <mertenslzm@gmail.com>
2020-06-06 18:53:04 +0800

    修改变量名

--- index.js
+++ index.js
@@ -42,11 +42,11 @@ function renderPlainText(data, plays) {
     return plays[aPerformance.playID];
   }
 
-  function volumeCreditsFor(perf) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
-    return volumeCredits;
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    return result;
   }
 
   function totalAmount() {
